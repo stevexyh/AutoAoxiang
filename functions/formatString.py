e7a55fd3ef661c590bc125671139dcf259706fb5
stevexyh@hotmail.com <stevexyh@hotmail.com>
2020-02-17 16:26:05 +0800

    修复了颜色跨平台显示问题

--- functions/formatString.py
+++ functions/formatString.py
@@ -24,7 +24,7 @@ def log_line(dic):
     for key in dic:
         flg = dic[key] is not None
         res = str(key).ljust(12,chr(12288))
-        res += ('\033[0;33;1m' + dic[key] + '\033[0m' if flg else '').ljust(20,chr(12288)) + '\n'
+        res += (setColor(dic[key], color = 'yellowFore') if flg else '').ljust(20,chr(12288)) + '\n'
     print(res)
 
 
@@ -37,11 +37,11 @@ def log_cn(dic):
     formLen = 40
 
     res = '-' * formLen + '\n'
-    res += datetime.datetime.now().strftime('[\033[0;32;1m%Y-%m-%d %H:%M:%S\033[0m]') + '\n'
+    res += '[' + setColor(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), color = 'greenFore') + ']\n'
     for key in dic:
         flg = dic[key] is not None
         res += str(key).ljust(12,chr(12288))
-        res += ('\033[0;33;1m' + dic[key] + '\033[0m' if flg else '').ljust(20,chr(12288)) + '\n'
+        res += (setColor(dic[key], color = 'yellowFore') if flg else '').ljust(20,chr(12288)) + '\n'
     res += '-' * formLen
     print(res)
 
@@ -54,28 +54,32 @@ def log_en(dic):
     formLen = 40
 
     res = '-' * formLen + '\n'
-    res += datetime.datetime.now().strftime('[\033[0;32;1m%Y-%m-%d %H:%M:%S\033[0m]') + '\n'
+    res += '[' + setColor(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), color = 'greenFore') + ']\n'
     for key in dic:
         flg = dic[key] is not None
         res += str(key).ljust(20)
-        res += ('\033[0;33;1m' + dic[key] + '\033[0m' if flg else '').ljust(20) + '\n'
+        res += (setColor(dic[key], color = 'yellowFore') if flg else '').ljust(20) + '\n'
     res += '-' * formLen
     print(res)
 
 
 def setColor(string, color):
-    if color == 'redFore':
-        return colorama.Fore.RED+ colorama.Back.RESET + string + colorama.Style.RESET_ALL
-    elif color == 'redBack':
-        return colorama.Fore.WHITE+ colorama.Back.RED + string + colorama.Style.RESET_ALL
-    elif color == 'greenFore':
-        return colorama.Fore.GREEN+ colorama.Back.RESET + string + colorama.Style.RESET_ALL
-    elif color == 'greenBack':
-        return colorama.Fore.BLACK+ colorama.Back.GREEN + string + colorama.Style.RESET_ALL
+    convertColor = {
+        'redFore': colorama.Fore.RED + colorama.Back.RESET, 
+        'redBack': colorama.Fore.WHITE + colorama.Back.RED,
 
-# if __name__ == "__main__":
-#     a = 'This is red.'
-#     b = setColor(a, 'redFore')
-#     print(b)
+        'greenFore': colorama.Fore.GREEN + colorama.Back.RESET,
+        'greenBack': colorama.Fore.BLACK + colorama.Back.GREEN,
 
-#     log_cn(logData)
+        'yellowFore': colorama.Fore.YELLOW + colorama.Back.RESET,
+    }
+
+    return colorama.Style.BRIGHT + convertColor[color] + string + colorama.Style.RESET_ALL
+
+
+if __name__ == "__main__":
+    a = 'This is red.'
+    b = setColor(a, 'redFore')
+    print(b)
+
+    log_cn(logData)
