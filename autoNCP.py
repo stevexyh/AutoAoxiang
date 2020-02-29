6dd8f332cfb748d41b9c552f7b47eaeca5b6183f
stevexyh@hotmail.com <stevexyh@hotmail.com>
2020-02-29 11:56:08 +0800

    同步更新了表单元素

--- autoNCP.py
+++ autoNCP.py
@@ -18,15 +18,15 @@ except ModuleNotFoundError:
     exit(-1)
 
 
-def submitForm(user = '', passwd = ''):
+def submitForm(user='', passwd=''):
     '''
     提交表格
     参数: 用户名, 密码, 所在地点
     '''
 
-    session = login_check(user = user, passwd = passwd)
+    session = login_check(user=user, passwd=passwd)
 
-    #表格url与提交url不同, 否则提交失败
+    # 表格url与提交url不同, 否则提交失败
     urlForm = 'http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp'
     urlSubmit = 'http://yqtb.nwpu.edu.cn/wx/ry/ry_util.jsp'
     global location
@@ -34,47 +34,51 @@ def submitForm(user = '', passwd = ''):
     if location == '':
         location = str(input('地点:'))
 
-    #表格请求头
+    # 表格请求头
     formHeaders = {
         'Origin': 'http://yqtb.nwpu.edu.cn',
         'Referer': 'http://yqtb.nwpu.edu.cn/wx/ry/jrsb.jsp',
         'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
     }
 
-    #填表信息
+    # 填表信息
     formData = {
         'actionType': 'addRbxx',
         'userLoginId': user,
 
-        #所在城市编码 / 名称
+        # 所在城市编码 / 名称
         'szcsbm': '3',
         'szcsmc': location,
 
-        #是否经停 / 说明
+        # 是否经停 / 说明
         'sfjt': '0',
         'sfjtsm': '',
 
-        #是否接触人员 / 说明
+        # 是否接触人员 / 说明
         'sfjcry': '0',
         'sfjcrysm': '',
-        
-        #是否接触确诊
+
+        # 是否接触确诊
         'sfjcqz': '0',
 
-        #是否有症状
+        # 是否有症状
         'sfyzz': '0',
 
-        #是否确诊 / 异常情况说明
+        # 是否确诊 / 异常情况说明
         'sfqz': '0',
         'ycqksm': '',
 
-        #隔离情况 / 隔离开始日期 / 隔离结束日期 / tbly和sso是啥意思？？？
+        # 健康情况 / 说明
+        'sfjkqk': '0',
+        'jkqksm': '',
+
+        # 隔离情况 / 隔离开始日期 / 隔离结束日期 / tbly和sso是啥意思？？？
         'glqk': '0',
         'glksrq': '',
         'gljsrq': '',
         'tbly': 'sso',
 
-        #本人承诺(此参数在服务器post里没找到)
+        # 本人承诺(此参数在服务器post里没找到)
         'brcn': '0',
     }
 
@@ -85,6 +89,7 @@ def submitForm(user = '', passwd = ''):
         '接触确诊疑似': '否',
         '今日体温': '37.2度以下',
         '有无疑似或异常': '无',
+        '您或家属健康状态': '正常',
         '是否隔离': '否',
     }
 
@@ -92,11 +97,11 @@ def submitForm(user = '', passwd = ''):
     session.get(urlForm)
 
     print('已获取表单, 准备提交...')
-    session.post(url = urlSubmit, data = formData, headers = formHeaders)
+    session.post(url=urlSubmit, data=formData, headers=formHeaders)
     res = session.get(urlForm).text
     session.close()
 
-    #成功信息, 此处为原网页设置, 不可随意更改
+    # 成功信息, 此处为原网页设置, 不可随意更改
     success = '您已成功提交今日上报，重新提交将覆盖上一次的信息。'
     status = res.find(success)
 
@@ -106,14 +111,14 @@ def submitForm(user = '', passwd = ''):
 
         global post_time, hrs
         post_time = datetime.datetime.now()
-        next_post = post_time + datetime.timedelta(hours = hrs)
+        next_post = post_time + datetime.timedelta(hours=hrs)
         next_dict = {'下次提交时间:': next_post.strftime('%Y-%m-%d %H:%M:%S')}
         fs.log_line(next_dict)
         print('-' * 100)
 
         return True
     else:
-        print(fs.setColor(string = '提交失败, 请重试', color = 'redBack'))
+        print(fs.setColor(string='提交失败, 请重试', color='redBack'))
         print(status)
         return False
 
@@ -126,10 +131,10 @@ if __name__ == "__main__":
 
     print(headerInfo)
 
-    username, password, location = get_info(is_input = False)
+    username, password, location = get_info(is_input=False)
     hrs = input('定时运行间隔时间(单位: 小时, 默认1):')
     hrs = 1 if hrs == '' else float(hrs)
 
     while True:
-        submitForm(user = username, passwd = password)
+        submitForm(user=username, passwd=password)
         sleep(hrs * 3600)
